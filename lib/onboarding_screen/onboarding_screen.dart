24723b899d782560dda8e7fb1d9e76219d98c9e3
Fahad Mehboob <fahad.fakky@hotmail.com>
2025-06-13 20:05:15 +0500

    native splash screen custom splash screen with animation completed onboarding screen require lottie or gif to complete

---
+++ lib/onboarding_screen/onboarding_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IntroductionScreen(
+        globalBackgroundColor: const Color(0xFFF9FAFB),
+        pages: [
+          PageViewModel(
+            title: "Schedule Matches",
+            body: "Easily find teams and book cricket matches around you.",
+            image: buildLottie(
+                "https://lottie.host/32dd1a94-c91b-4d62-9a4c-f151b2e44b70/af1snlaIFk.json"),
+            decoration: pageDecoration(),
+          ),
+          PageViewModel(
+            title: "Manage Teams",
+            body: "Handle team players, match info, and share results.",
+            image: buildLottie(
+                "https://lottie.host/0277e123-0d49-4ad8-a84d-e5e1c36f52b1/37Lu2UXc2n.json"),
+            decoration: pageDecoration(),
+          ),
+          PageViewModel(
+            title: "Join Cricket Community",
+            body: "Connect with other local cricket lovers & clubs.",
+            image: buildLottie(
+                "https://lottie.host/77c0e8c8-d8b1-4432-a9a2-f8f6ee8cf470/Qm9Eu7ql9c.json"),
+            decoration: pageDecoration(),
+            footer: ElevatedButton(
+              onPressed: () => goToHome(context),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 14),
+              ),
+              child: const Text(
+                "Get Started",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
+        onDone: () => goToHome(context),
+        onSkip: () => goToHome(context),
+        showSkipButton: true,
+        skip: const Text("Skip", style: TextStyle(color: Colors.deepPurple)),
+        next: const Icon(Icons.arrow_forward, color: Colors.deepPurple),
+        done: const Text("Done",
+            style: TextStyle(
+                fontWeight: FontWeight.w600, color: Colors.deepPurple)),
+        dotsDecorator: DotsDecorator(
+          size: const Size(10, 10),
+          color: Colors.grey.shade300,
+          activeColor: Colors.deepPurple,
+          activeSize: const Size(22, 10),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        animationDuration: 500,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
+  }
+
+  void goToHome(BuildContext context) {
+    // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const HomeScreen()));
+  }
+
+  Widget buildLottie(String url) => Center(
+        child: Lottie.network(url, width: 300, fit: BoxFit.contain),
+      );
+
+  PageDecoration pageDecoration() => PageDecoration(
+        titleTextStyle: GoogleFonts.poppins(
+          fontSize: 26,
+          fontWeight: FontWeight.bold,
+          color: Colors.deepPurple,
+        ),
+        bodyTextStyle: GoogleFonts.poppins(
+          fontSize: 16,
+          color: Colors.grey.shade700,
+        ),
+        imagePadding: const EdgeInsets.all(16),
+        contentMargin: const EdgeInsets.all(16),
+        pageColor: const Color(0xFFF9FAFB),
+      );
+}
--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashScreen extends StatelessWidget {
+import '../onboarding_screen/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(_controller);
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.offAll(() => const OnboardingScreen());
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xFF94A3B8),
+      body: Center(
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                height: 350,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
